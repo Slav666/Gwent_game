f1535d9245ba67bfb321971e099fd1a3f93884d6
Slav666 <slawomirdyk@gmail.com>
2019-06-29 22:25:50 +0100

    cards array added and recealCard funcion created

---
+++ memory.js
@@ -0,0 +1,2 @@
+var cards  = ["ciri.png", "geralt.png", "jaskier.png", "jaskier.png", "iorewth.png", "triss.png", "geralt.png", "yen.png", "ciri.png", "triss.png", "yen.png", "iorweth.png"];
+console.log(cards);
